8530e174a417b3e3798714f27f541340cb675fe1
Jarcoz <juan.arcos.hdz@gmail.com>
2020-10-24 17:28:11 -0500

    Initial Commit

---
+++ KibasDefense/Assets/BulletMock.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class BulletMock : MonoBehaviour
+{
+
+    private Transform target;
+
+    public float speed = 70f;
+
+    public void Seek(Transform _target)
+    {
+        target = _target;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target == null)
+        {
+            Debug.Log("Destroy bullet");
+            Destroy(gameObject);
+            return;
+        }
+
+        Vector3 dir = target.position - transform.position;
+        float distanceThisFrame = speed * Time.deltaTime;
+
+
+        Debug.Log("bullet + dir.magnitude: " + dir.magnitude);
+        if (dir.magnitude <= distanceThisFrame){
+            
+        }
+
+    }
+}
